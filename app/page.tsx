a73b51ac8468b96f3d4b20479f5228a5b16ddb05
Nacim <nacim9190@gmail.com>
2026-05-06 22:14:15 +0200

    fix: import relatif Hero pour corriger build Vercel

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 'use client'
 
 import Navbar from '@/components/Navbar'
-import Hero from '@/components/Hero'   // ← Import correct
+import Hero from '../components/Hero'     // ← Import relatif (corrigé)
 
 export default function Home() {
   return (
